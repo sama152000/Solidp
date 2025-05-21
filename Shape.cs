6e1aa7b05bcb45d11201275c20628ae51942a685
sama <sama.sama152000@gmail.com>
2025-05-21 16:48:34 +0300

    ...

---
+++ Shape.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+public interface IShape
+{
+    double Area();
+}
+
+public interface IVolumetricShape
+{
+    double Volume();
+}
+
+public class Rectangle : IShape
+{
+    public double Height { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public double Area()
+    {
+        return Height * Width;
+    }
+}
+
+public class Square : IShape
+{
+    public double Side { get; set; }
+
+    public Square(double side)
+    {
+        Side = side;
+    }
+
+    public double Area()
+    {
+        return Side * Side;
+    }
+}
+
+public class Triangle : IShape
+{
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public Triangle(double @base, double height)
+    {
+        Base = @base;
+        Height = height;
+    }
+
+    public double Area()
+    {
+        return 0.5 * Base * Height;
+    }
+}
+
+public class Circle : IShape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double Area()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Cube : IShape, IVolumetricShape
+{
+    public double Side { get; set; }
+
+    public Cube(double side)
+    {
+        Side = side;
+    }
+
+    public double Area()
+    {
+        return 6 * Side * Side; 
+    }
+
+    public double Volume()
+    {
+        return Side * Side * Side;
+    }
+}
+
+public class AreaCalculator
+{
+    public double TotalArea(IShape[] shapes)
+    {
+        double area = 0;
+        foreach (var shape in shapes)
+        {
+            area += shape.Area(); 
+        }
+        return area;
+    }
+}
+
+public class VolumeCalculator
+{
+    public double TotalVolume(IVolumetricShape[] shapes)
+    {
+        double volume = 0;
+        foreach (var shape in shapes)
+        {
+            volume += shape.Volume();
+        }
+        return volume;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class User
+{
+    public string Email { get; }
+    public string Password { get; }
+
+    public User(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message) : base(message) { }
+}
+
+public interface IEmailValidator
+{
+    bool ValidateEmail(string email);
+}
+
+public interface IEmailSender
+{
+    bool SendEmail(MailMessage message);
+}
+
+public class EmailValidator : IEmailValidator
+{
+    public bool ValidateEmail(string email)
+    {
+        return email.Contains("@"); 
+    }
+}
+
+public class EmailSender : IEmailSender
+{
+    private readonly SmtpClient _smtpClient;
+
+    public EmailSender(SmtpClient smtpClient)
+    {
+        _smtpClient = smtpClient;
+    }
+
+    public bool SendEmail(MailMessage message)
+    {
+        _smtpClient.Send(message);
+        return true;
+    }
+}
+
+public class UserService
+{
+    private readonly IEmailValidator _emailValidator;
+    private readonly IEmailSender _emailSender;
+
+    public UserService(IEmailValidator emailValidator, IEmailSender emailSender)
+    {
+        _emailValidator = emailValidator;
+        _emailSender = emailSender;
+    }
+
+    public void Register(string email, string password)
+    {
+        if (!_emailValidator.ValidateEmail(email))
+            throw new ValidationException("Email is not valid");
+
+        var user = new User(email, password);
+
+        _emailSender.SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject = "Welcome!" });
+    }
+}
+
+public class MailMessage
+{
+    public string From { get; }
+    public string To { get; }
+    public string Subject { get; set; }
+
+    public MailMessage(string from, string to)
+    {
+        From = from;
+        To = to;
+    }
+}
+
+public class SmtpClient
+{
+    public void Send(MailMessage message)
+    {
+        Console.WriteLine($"Sending email to {message.To} with subject: {message.Subject}");
+    }
+}
